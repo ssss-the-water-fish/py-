462ba50c607a63d8972610d9e50a0b054e993cec
ssss-the-water-fish <63834613+ssss-the-water-fish@users.noreply.github.com>
2020-11-19 09:36:42 +0000

    Delete ？？？（谢岳宏）.py

--- ？？？（谢岳宏）.py
+++
@@ -1,9 +0,0 @@
-#5*a+3*b+3*c=100
-#a+b+c=100
-for a in range(0,20):
-    for b in range(0,33):
-        for c in range(0,300):
-            if a+b+c==100 and 5*a+3*b+(1/3)*c==100:
-                print(a,b,c)
-input()
-exit()
